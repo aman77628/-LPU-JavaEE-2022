ec821146306dd702d92a960645b079f6279dda99
Aman <63458771+aman77628@users.noreply.github.com>
2022-01-27 20:37:37 +0530

    Add files via upload
    SESSION 1

---
+++ MyApp.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author 91705
+ *
+ */
+public class MyApp {
+
+	/**
+	 * @param args : it is commamnd line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Thank You All for being wonderful audience");
+		System.out.println("I will see you ALL  tommorow");
+		System.out.println("Code the same what we did in the class");
+	
+
+	}
+
+}
